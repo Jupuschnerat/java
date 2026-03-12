1fb43a5e59dbeacd28c4b3bf5389f30bfe003c1c
JuPuschnerat <jupuschnerat@gmail.com>
2026-03-12 06:51:00 -0300

    commit inicial

---
+++ estruturaSequencial/ex-00/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+    int minhaIdade = 25;
+    String meuNome = "Júlia";
+
+    System.out.println("Olá, meu nome é " + meuNome + " e eu tenho " + minhaIdade + " anos.");
+    }   
+}
